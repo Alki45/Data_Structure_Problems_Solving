c7a5735b800428bfe6d2a5e85e4f19335f54ab2f
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-02-04 22:41:38 +0300

    Time: 23 ms (49.9%), Space: 19 MB (8.31%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -2,9 +2,8 @@ class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         Product=1
         nums.sort()
-        if(len(nums)>=3):
-            If_Positive=nums[-1]*nums[-2]*nums[-3]
-            If_Negative=max(nums[0]*nums[1]*nums[2],nums[0]*nums[1]*nums[-1])
-            Product=max(If_Positive,If_Negative)
+        If_Positive=nums[-1]*nums[-2]*nums[-3]
+        If_Negative=max(nums[0]*nums[1]*nums[2],nums[0]*nums[1]*nums[-1])
+        Product=max(If_Positive,If_Negative)
 
         return Product
