19ca56f481e63c7a95f72b9af7f1fa4ce52dd522
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-02-26 15:00:48 +0300

    Time: 12 ms (91.79%), Space: 30.8 MB (68.57%) - LeetHub

---
+++ 1459-apply-discount-every-n-orders/1459-apply-discount-every-n-orders.py
@@ -0,0 +1,28 @@
+class Cashier:
+
+    def __init__(self, n: int, discount: int, products: List[int], prices: List[int]):
+        self.price=dict(zip(products,prices))
+        self.discount=discount
+        self.n=n
+        self.customer_count=0
+
+
+    def getBill(self, product: List[int], amount: List[int]) -> float:
+        self.customer_count += 1
+        subtotal = 0
+        for i,p in enumerate(product):
+            subtotal += self.price[p]* amount[i]
+
+        if self.customer_count % self.n == 0:
+            subtotal *= (100 - self.discount) / 100
+
+        return round(subtotal, 2)
+
+        
+
+
+# Your Cashier object will be instantiated and called as such:
+
+# obj = Cashier(n, discount, products, prices)
+
+# param_1 = obj.getBill(product,amount)
