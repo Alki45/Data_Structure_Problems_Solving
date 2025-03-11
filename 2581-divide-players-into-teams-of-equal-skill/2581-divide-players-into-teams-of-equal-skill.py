fbf984d7d6a47c36db6c4734c6f574181e296b40
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-03-11 20:27:41 +0300

    Time: 32 ms (88.9%), Space: 28.9 MB (27.72%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -7,14 +7,12 @@ class Solution:
         skill.sort()
         skill_team={}
         target=skill[1]+skill[-2]
+        product=0
         while (l<r):
             if(skill[l]+skill[r]==target):
-                skill_team[l]=tuple([skill[l],skill[r]])
+                product+=skill[l]*skill[r]
+            else:
+                return -1
             l+=1
             r-=1
-        if(len(skill_team)==len(skill)/2):
-            product=0
-            for key,val in skill_team.items():
-                product+=val[0]*val[1]
-            return product
-        return -1
+        return product
