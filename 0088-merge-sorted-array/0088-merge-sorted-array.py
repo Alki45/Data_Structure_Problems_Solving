abe0a85cd0b2b2709f6aed1dde5a40d3881f0618
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-03-19 00:21:14 +0300

    Time: 0 ms (100%), Space: 17.8 MB (71.95%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,36 +2,28 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-                i=0
-        i, j, k = m - 1, n - 1, m + n - 1
-
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
 
         """
-        """        left,right,i=0,0,0
+        left,right,i=0,0,0
+        temp=nums1[:m]
+        print(temp)
         while left<m and right <n:
-            if(nums1[left]<nums2[right]):
-                nums1[i]=nums1[left]
+            if(temp[left]<nums2[right]):
+                nums1[i]=temp[left]
                 left+=1
             else:
-                nums1[i],nums2[right]=nums2[right],nums1[left]
+                nums1[i]=nums2[right]
                 right+=1
-            i+=1"""
-        for i in range(len(nums2)):
-                nums1.append(nums2[i])
-        nums1.sort()
-        for i in range (n):
-                nums1.remove(0)
-            
+            i+=1
+        while right<n:
+                nums1[i]=nums2[right]
+                right+=1
+                i+=1
+        while left<m:
+                nums1[i]=temp[left]
+                left+=1
+                i+=1
+
+
+
 
