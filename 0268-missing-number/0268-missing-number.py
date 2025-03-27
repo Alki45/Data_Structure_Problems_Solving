d3e45ff88ed2f71207e8e21e41d7e98f8acf4311
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-03-27 18:09:37 +0300

    Time: 8 ms (31.45%), Space: 19.9 MB (6.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        dict_num={num:num for num in range(len(nums)+1)}
-        nums.sort()
-        new_list=[num for num in dict_num.values()]
+        new_list=[num for num in range(len(nums)+1)]
         return (list(set(new_list) - set(nums))[0])
 
         
