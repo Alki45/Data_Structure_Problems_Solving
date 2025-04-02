bf2289f7b1bb7d01603dd840ba73940e35461bd3
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-04-02 11:18:43 +0300

    Time: 5 ms (64.71%), Space: 19.4 MB (19.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,10 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        Dic_nums=Counter(nums)
-        Max_mod=max(Dic_nums.values())
-        for key,value in Dic_nums.items():
-            if value ==Max_mod:
+        Dict_num=Counter(nums)
+        Max_freq=0
+        for key,value in Dict_num.items():
+            Max_freq=max(Max_freq,value)
+        for key,value in Dict_num.items():
+            if(Dict_num[key]==Max_freq):
                 return key
         
