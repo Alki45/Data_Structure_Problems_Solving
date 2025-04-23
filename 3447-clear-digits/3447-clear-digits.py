1d5b44eb9b1a59f81aa7e8fc6f4369f782f09730
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-04-23 09:19:12 +0300

    Time: 5 ms (4.96%), Space: 18 MB (8.25%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        strs=list(s)
+        stack=[]
+        l=0
+
+
+        while l<len(strs):
+            if(strs[l].isdigit()):
+                strs.pop(l)
+                strs.pop(l-1)
+                l-=1
+            else:
+                l+=1
+        return "".join(strs)
+
+        
