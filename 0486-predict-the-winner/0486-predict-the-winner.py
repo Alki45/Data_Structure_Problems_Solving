aac30b1d43000880508cd393195fa3adb02c142d
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-04-30 17:43:23 +0300

    Time: 2373 ms (24.02%), Space: 17.7 MB (73.34%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,10 +1,11 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def helper(left, right):
+        def Max_Pick(left, right):
             if left == right:
                 return nums[left]
-            pick_left = nums[left] - helper(left + 1, right)
-            pick_right = nums[right] - helper(left, right - 1)
+
+            pick_left = nums[left] - Max_Pick(left + 1, right)
+            pick_right = nums[right] - Max_Pick(left, right - 1)
             return max(pick_left, pick_right)
         
-        return helper(0, len(nums) - 1) >= 0
+        return Max_Pick(0, len(nums) - 1) >= 0
