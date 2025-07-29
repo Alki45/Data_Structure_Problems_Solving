2b49651868fc1a46d12e9ed73772b65344f5248f
Ali Kibret <135044501+Alki45@users.noreply.github.com>
2025-07-29 18:13:22 +0300

    Time: 0 ms (100%), Space: 17.6 MB (78.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self):
+        self.memo = defaultdict(int)
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return n
+        if n == 2:
+            return n
+        if n not in self.memo:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n]
+        
+        
